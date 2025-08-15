d5cfcb6453727746cb433a28c69285e4a2dcc03f
ZGh019 <ziadghoul53@gmail.com>
2025-08-15 23:31:58 +0400

    Update home.js

--- script/home.js
+++ script/home.js
@@ -1,28 +1,25 @@
+// تاثرات الشريط العلوي بعد الدفع يتقلص 
 document.addEventListener('DOMContentLoaded', function() {
-    const header = document.querySelector('.top-bar');
-
-    // تأثير الذهب عند التحميل
-    header.classList.add('animate-gold');
-    setTimeout(() => {
-        header.classList.remove('animate-gold');
-    }, 4000);
-
-    // تأثير التقلص عند التمرير
     window.addEventListener('scroll', function() {
+        const header = document.querySelector('.top-bar');
         if (window.scrollY > 50) {
-            header.classList.add('shrink', 'animate-gold');
+            header.classList.add('shrink');
         } else {
-            header.classList.remove('shrink', 'animate-gold');
+            header.classList.remove('shrink');
         }
     });
 });
 
+// انميشا الشريط العلوي 
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('.top-bar');
+    header.classList.add('animate-gold');
 
-    //window.addEventListener('scroll', function() {
-    //    if (window.scrollY > 50) {
-    //       header.classList.add('shrink', 'animate-gold');
-    //    } else {
-    //        header.classList.remove('shrink', 'animate-gold');
-     //   }
-  //  });
-//});
+    // إزالة الحركة بعد 4 ثواني
+    setTimeout(() => {
+        header.classList.remove('animate-gold');
+    }, 4000);
+});
+
+
+    
