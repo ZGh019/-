6886ace6b4634b842f1f5ce58f73e15d6e965e40
ZGh019 <ziadghoul53@gmail.com>
2025-08-15 23:19:18 +0400

    Update home.js

--- script/home.js
+++ script/home.js
@@ -1,10 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('.top-bar');
+
+    // تأثير الذهب عند التحميل
+    header.classList.add('animate-gold');
+    setTimeout(() => {
+        header.classList.remove('animate-gold');
+    }, 4000);
+
+    // تأثير التقلص عند التمرير
     window.addEventListener('scroll', function() {
-        const header = document.querySelector('.top-bar');
         if (window.scrollY > 50) {
-            header.classList.add('shrink');
+            header.classList.add('shrink', 'animate-gold');
         } else {
-            header.classList.remove('shrink');
+            header.classList.remove('shrink', 'animate-gold');
         }
     });
 });
+
+
+    //window.addEventListener('scroll', function() {
+    //    if (window.scrollY > 50) {
+    //       header.classList.add('shrink', 'animate-gold');
+    //    } else {
+    //        header.classList.remove('shrink', 'animate-gold');
+     //   }
+  //  });
+//});
